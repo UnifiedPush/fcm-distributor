f10629257a250819ad329cffc05fb089437aa4b5
sim <git@sgougeon.fr>
2022-06-11 12:31:00 +0200

    Handle split messages
    Fix #56

--- app/src/main/java/org/unifiedpush/distributor/fcm/services/FirebaseForwardingService.kt
+++ app/src/main/java/org/unifiedpush/distributor/fcm/services/FirebaseForwardingService.kt
@@ -7,10 +7,17 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import org.unifiedpush.distributor.fcm.services.PushUtils.sendEndpoint
 import org.unifiedpush.distributor.fcm.services.PushUtils.sendMessage
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 private const val TAG = "FirebaseForwardingService"
 
 class FirebaseForwardingService : FirebaseMessagingService() {
+
+    companion object {
+        val pendingMessages = mutableMapOf<String, ByteArray>()
+    }
+
     override fun onNewToken(token: String) {
         Log.d(TAG, "Firebase onNewToken $token")
         val settings = baseContext.getSharedPreferences("Config", Context.MODE_PRIVATE)
@@ -25,8 +32,41 @@ class FirebaseForwardingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d(TAG, "Firebase onMessageReceived ${remoteMessage.messageId}")
-        val message = Base64.decode(remoteMessage.data["b"]!!, Base64.DEFAULT)
         val appToken = remoteMessage.data["i"]!!
-        sendMessage(baseContext, appToken, message)
+        getMessage(remoteMessage.data)?.let { message ->
+            sendMessage(baseContext, appToken, message)
+        }
+    }
+
+    private fun getMessage(data: MutableMap<String, String>): ByteArray? {
+        var message: ByteArray? = null
+        data["b"]?.let { b64 ->
+            data["m"]?.let { mId ->
+                data["s"]?.let { splitId ->
+                    if (pendingMessages.containsKey(mId)) {
+                        Log.d(TAG, "Found pending message")
+                        when(splitId) {
+                            "1" -> {
+                                message = Base64.decode(b64, Base64.DEFAULT) +
+                                        pendingMessages[mId]!!
+                            }
+                            "2" -> {
+                                message = pendingMessages[mId]!! +
+                                        Base64.decode(b64, Base64.DEFAULT)
+                            }
+                        }
+                        pendingMessages.remove(mId)
+                    } else {
+                        pendingMessages[mId] = Base64.decode(b64, Base64.DEFAULT)
+                        Timer().schedule(3000) {
+                            pendingMessages.remove(mId)
+                        }
+                    }
+                }
+            }?:run {
+                message = Base64.decode(b64, Base64.DEFAULT)
+            }
+        }
+        return message
     }
 }
