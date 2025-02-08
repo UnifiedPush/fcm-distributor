abe97ecf022ff4131f34291b2304f71f3549c993
sim <git@sgougeon.fr>
2025-02-08 10:23:10 +0000

    Make main UI scrollable

--- app/src/main/java/org/unifiedpush/distributor/fcm/activities/ui/MainUi.kt
+++ app/src/main/java/org/unifiedpush/distributor/fcm/activities/ui/MainUi.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -58,7 +60,8 @@ fun MainUiContent(viewModel: MainViewModel, innerPadding: PaddingValues) {
                 innerPadding.calculateTopPadding(),
                 0.dp,
                 innerPadding.calculateBottomPadding()
-            ),
+            )
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
